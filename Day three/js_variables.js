ffe23043821a208bb65a0029683d7406d140f95d
JMorgan-git <103576877+JMorgan-git@users.noreply.github.com>
2022-05-18 12:40:10 +0100

    Add files via upload

--- Day three/js_variables.js
+++ Day three/js_variables.js
@@ -21,11 +21,11 @@ let b = "12345";
 let c = 12344;
 let d = true;
 let e = {a:"JavaScript"};
-typeof(a);
-typeof(b);
-typeof(c);
-typeof(d);
-typeof(e);
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
 
 let totalMoney = 4000;
 let moneyPaidSoFar = 2348;
