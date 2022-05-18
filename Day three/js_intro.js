a224248d4bb5110beafdad2525a89477b971bf3a
JMorgan-git <103576877+JMorgan-git@users.noreply.github.com>
2022-05-18 12:25:18 +0100

    Task one and Task Two

---
+++ Day three/js_intro.js
@@ -0,0 +1,30 @@
+console.log("Task One")
+// logging levels
+// log level
+console.log("First Name");
+// info level
+console.info("Second Name");
+// warning level
+console.warn("Where are you from");
+// error level
+console.error("And your star sign");
+
+let make = "Audi";
+let model = "A5";
+console.log("My Favourite Car is " + make + " and the model is " + model);
+
+let myTxt = "This is a message";
+
+console.log("%c"+myTxt,"color: orange; font-family: fantasy;font-style:bolder; background-color: black; padding: 10px");
+
+
+// // variables(starting with letter"_""$" case sensitive)
+// const myNumber = 5;
+// //myNumber = 10;
+// console.log(myNumber);
+// // const cannot be reassigned
+
+// let myNumber2 = 4;
+// myNumber2 = 10;
+// console.log(myNumber2);
+// //let can be reassigned
---
+++ Day three/js_variables.js
@@ -0,0 +1,34 @@
+console.log("Task Two")
+
+// let myNullValue = null;
+// console.log(myNullValue);
+// console.log(typeof myNullValue);
+// let myUndefinedValue = undefined;
+// console.log(myUndefinedValue);
+// console.log(typeof myUndefinedValue);
+// console.log(myNullValue == undefined);
+// console.log(myNullValue === undefined);
+// console.log("10" == 10); // good old truthy falsey
+
+// let name = "Rowan";
+// let myString = "Hello there" + person;
+
+// let myTemplateLiteral = `Hello there ${person}`;
+// console.log(myTemplateLiteral);
+
+let a;
+let b = "12345";
+let c = 12344;
+let d = true;
+let e = {a:"JavaScript"};
+typeof(a);
+typeof(b);
+typeof(c);
+typeof(d);
+typeof(e);
+
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay;
+
+console.log(`The total bill is ${totalMoney} the remaining amount of money to be paid is ${totalMoney-moneyPaidSoFar}`);
